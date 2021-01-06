44e34f278f5f76b9b2a83799189ffd518016c1a4
DragSama <49992845+DragSama@users.noreply.github.com>
2021-01-06 12:38:12 +0530

    Add missing doc string

--- attrify/__init__.py
+++ attrify/__init__.py
@@ -17,6 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""Import Attrify and declare __version__ & __author__ variable."""
 
 from .attrified_dict import Attrify
 
